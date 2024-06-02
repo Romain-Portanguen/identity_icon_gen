91a7685773664e3fa9b5f3d5a8cd047d0a8120a4
Romain Portanguen <romain.portanguen@merci-app.com>
2024-06-02 13:29:52 +0200

    Refactoring | Updating the global style | Integrating a viewer for the svg code generated by the seed

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { AvatarGenerator } from './components/AvatarGenerator';
+import { SvgCodeViewer } from './components/SVGViewer';
 
-const Container = styled.div`
+const AppContainer = styled.div`
   align-items: center;
   display: flex;
   flex-direction: column;
-  height: 100vh;
   padding: 20px;
-  width: 100vw;
 `;
 
-const Title = styled.h1`
-  color: #fff;
+const AppTitle = styled.h1`
+  background-color: #007bff;
+  border-radius: 6px;
+  border: 1px solid #009bff;
+  box-shadow: 0 0 10px #007bff;
   font-family: 'Roboto', sans-serif;
   font-size: 36px;
-  font-weight: 600;
-  line-height: 1.2;
-  margin-bottom: 20px;
-  margin-top: 0;
+  font-weight: 700;
+  text-align: center;
+  width: 100%;
 `;
 
-const App: React.FC = () => (
-  <Container>
-    <Title>Identity icon generator</Title>
-    <AvatarGenerator />
-  </Container>
-);
+const ContentContainer = styled.div`
+  display: flex;
+  gap: 20px;
+  justify-content: center;
+  margin: 20px;
+  width: 100%;
+`;
+
+const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  max-width: 50%;
+`;
+
+const Footer = styled.div`
+  margin-top: 20px;
+  padding: 20px;
+
+  p {
+    font-family: 'Roboto', sans-serif;
+    font-size: 16px;
+    text-align: center;
+  }
+
+  a {
+    color: #007bff;
+    text-decoration: none;
+
+    &:hover {
+      cursor: pointer;
+      text-decoration: underline;
+    }
+  }
+`;
+
+const App: React.FC = () => {
+  const [svgCode, setSvgCode] = useState<string>('');
+
+  return (
+    <AppContainer>
+      <AppTitle>IIG - Identity Icon Generator</AppTitle>
+      <ContentContainer>
+        <Column>
+          <AvatarGenerator setSvgCode={setSvgCode} />
+        </Column>
+        <Column>
+          <SvgCodeViewer svgCode={svgCode} />
+        </Column>
+      </ContentContainer>
+      <Footer>
+        <p>
+          Made by{' '}
+          <a href="https://github.com/Romain-Portanguen" target="_blank" rel="noreferrer">
+            Romain Portanguen - Software Engineer
+          </a>
+        </p>
+      </Footer>
+    </AppContainer>
+  );
+};
 
 export default App;
--- src/components/AvatarGenerator.tsx
+++ src/components/AvatarGenerator.tsx
@@ -8,17 +8,18 @@ import DownloadButton from './DownloadButton';
 
 const Container = styled.div`
   align-items: center;
+  border-radius: 4px;
+  border: 1px solid #009bff;
+  box-shadow: 0 0 10px #007bff;
   display: flex;
   flex-direction: column;
-  height: -webkit-fill-available;
-  justify-content: flex-start;
-  padding: 20px;
-  width: 100%;
 `;
 
 const CanvasContainer = styled.div`
   align-items: center;
-  border: none;
+  border-radius: 6px;
+  border: 1px solid #009bff;
+  box-shadow: 0 0 10px #007bff;
   display: flex;
   height: 256px;
   justify-content: center;
@@ -27,8 +28,7 @@ const CanvasContainer = styled.div`
 `;
 
 const Canvas = styled.canvas`
-  border-radius: 6px;
-  border: 1px solid black;
+  border-radius: 4px;
   height: 256px;
   width: 256px;
 `;
@@ -40,12 +40,17 @@ const DownloadButtonWrapper = styled.div`
   margin-top: 20px;
 `;
 
-export const AvatarGenerator: React.FC = () => {
+interface AvatarGeneratorProps {
+  setSvgCode: (svgCode: string) => void;
+}
+
+export const AvatarGenerator: React.FC<AvatarGeneratorProps> = ({ setSvgCode }) => {
   const [canvasUrl, setCanvasUrl] = useState<string | null>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   const generateAvatar = useCallback(() => {
     const canvas = canvasRef.current;
+
     if (canvas) {
       const opts: IconOpts = {
         size: 256,
@@ -53,7 +58,28 @@ export const AvatarGenerator: React.FC = () => {
       };
       renderIcon(buildOpts(opts), canvas);
       setCanvasUrl(canvas.toDataURL());
+
+      const context = canvas.getContext('2d');
+      const imageData = context?.getImageData(0, 0, canvas.width, canvas.height);
+
+      if (imageData) {
+        const svgCode = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="${canvas.width}" height="${canvas.height}">
+          <foreignObject width="100%" height="100%">
+            <div xmlns="http://www.w3.org/1999/xhtml">
+              <img src="${canvas.toDataURL()}" alt="Identity Icon" />
+            </div>
+          </foreignObject>
+        </svg>
+      `;
+
+        const encodedSvgCode = btoa(svgCode);
+        const decodedSvgCode = atob(encodedSvgCode);
+
+        setSvgCode(decodedSvgCode);
+      }
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
@@ -66,12 +92,12 @@ export const AvatarGenerator: React.FC = () => {
       <CanvasContainer>
         <Canvas ref={canvasRef} />
       </CanvasContainer>
-      {canvasUrl && (
+      {canvasUrl ? (
         <DownloadButtonWrapper>
           <DownloadButton canvasRef={canvasRef} format="png" />
           <DownloadButton canvasRef={canvasRef} format="jpg" />
         </DownloadButtonWrapper>
-      )}
+      ) : null}
     </Container>
   );
 };
